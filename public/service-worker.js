949bdbc4c33f21437a746f5b3c9c7f8151694c49
Christopher Langner <langnerc@icloud.com>
2022-07-14 10:53:59 -0500

    works offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,79 +1,67 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/idb.js",
-    "/index.js",
-    "/manifest.json",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-  ];
-  
-  const CACHE_NAME = "static-cache-v22";
-  const DATA_CACHE_NAME = "data-cache-v33";
-  
-  // install
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  // activate
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(function() {
-      return caches.match(evt.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
+  "/",
+  "/index.html",
+  "/assets/css/styles.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
-  });
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request, response.clone());
+              }
+
+              return response;
+            })
+            .catch(() => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const apiRoutes = require("./routes/api")
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
@@ -16,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: true
 });
